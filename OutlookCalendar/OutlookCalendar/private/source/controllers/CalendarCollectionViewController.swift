e659fb70a55e7d089add6d766ccd3e9017cfe713
Roopa Natarajan <>
2018-03-31 16:14:39 -0700

    Add a CollectionVC for the calendar view

---
+++ OutlookCalendar/OutlookCalendar/private/source/controllers/CalendarCollectionViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CalendarCollectionViewController.swift
+//  OutlookCalendar
+//
+//  Created by Roopa Natarajan on 3/31/18.
+//  Copyright © 2018 Microsoft. All rights reserved.
+//
+
+import Foundation
--- OutlookCalendar/OutlookCalendar/private/source/controllers/ViewController.swift
+++ OutlookCalendar/OutlookCalendar/private/source/controllers/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  OutlookCalendarViewController.swift
 //  OutlookCalendar
 //
 //  Created by Roopa Natarajan on 3/31/18.
@@ -8,18 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class OutlookCalendarViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
+    
 
 }
 
