2db01d03b3da6ef666bc1bf068cc1dcb13cd8bd8
Roopa Natarajan <>
2018-04-08 00:30:27 -0700

    Update README and add a json parser test

--- OutlookCalendar/OutlookCalendarTests/OutlookCalendarTests.swift
+++ OutlookCalendar/OutlookCalendarTests/OutlookCalendarTests.swift
@@ -21,8 +21,15 @@ class OutlookCalendarTests: XCTestCase {
         super.tearDown()
     }
     
+    func testJsonParser() {
+        // Verify that the json parser, parses the static input and returns an array of 101 values
+        let array = JsonParser.JSONParseArray()
+        XCTAssertNotNil(array)
+        XCTAssert(array?.count == 101)
+    }
+
     func testCoreDataSave() {
-        let testJson = jsonResponseForTest()
+        let testJson = singleJsonResponseForTest()
         // Save the json response to core data model
         _ = ObjectStore.sharedInstance.createOrUpdateEventEntityFrom(elementInfo: testJson)
         do {
@@ -62,7 +69,7 @@ class OutlookCalendarTests: XCTestCase {
         XCTAssert(DateTimeUtil.weekdayFromDate(date: date) == 7)
     }
 
-    func jsonResponseForTest() -> AnyObject {
+    func singleJsonResponseForTest() -> AnyObject {
         return "{\"eventId\":\"2252bcaf-21f4-4c23-901c-b928f5ad189b\",\"eventTitle\":\"meeting58\",\"eventDescription\":\"My own description, Id:5\",\"eventDateTimeUTC\":\"2018-02-26T01:06:14\",\"eventDuration\":\"88\",\"location\":{\"locationId\":\"4036fb00-d290-4c4e-a806-78bb4b642077\",\"buildingNumber\":\"58\",\"roomNumber\":\"10\"},\"attendees\":[{\"attendeeId\":\"dfa956eb-316c-481e-b8f6-fae3118d4941\",\"person\":{\"personId\":\"0aec199d-36cb-475e-b7bb-4add89e22a8b\",\"firstName\":\"f29-myFirstName-19f\",\"lastName\":\"l2-MyLastName-10h\",\"alias\":\"f29-e-10h\"},\"status\":\"organizer\"},{\"attendeeId\":\"d4f7a1cd-e0c1-4988-bdc3-08c34c67cfba\",\"person\":{\"personId\":\"fc129453-5472-4794-80f5-af6c9ae31fdc\",\"firstName\":\"z0-myFirstName-18n\",\"lastName\":\"w22-MyLastName-4n\",\"alias\":\"z0-mme-4n\"},\"status\":\"accepted\"},{\"attendeeId\":\"a48d7c09-a1a8-491c-9b05-09a5d2eefd45\",\"person\":{\"personId\":\"64bb5893-0b77-4f59-bd7c-56855f58e52c\",\"firstName\":\"w18-myFirstName-0k\",\"lastName\":\"i19-MyLastName-22o\",\"alias\":\"w18-e-22o\"},\"status\":\"accepted\"},{\"attendeeId\":\"e0066080-84bd-4440-aeaf-2b3b776e68ca\",\"person\":{\"personId\":\"0860247a-9aae-4c94-887b-a9e85563b218\",\"firstName\":\"z10-myFirstName-25w\",\"lastName\":\"v29-MyLastName-14t\",\"alias\":\"z10-e-14t\"},\"status\":\"accepted\"}]}" as AnyObject
     }
     
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # outlook-calendar
+
+# More potential TO-DOs
+
+I have already mentioned some TO-DO s in the code comments which also include some bugs (search for TO-DO)
+
+1.  I need to move all constants (strings, UX dimensions etc. to separate const files)
+
+2.  Localization - Strings have to be localized and separate resource files have to be created for all supported languages
+
+3.  Accessibility - Accessibility has to be built into the app, right now I just gave identifiers for a couple of elements to be used in tests
+
+4.  Tests - This is the first time I am trying to use the UI tests and I am kind of apprehensive about its stability after some experiments.
+	Over my years at Microsoft, I have seen/fixed my share of tests and I believe visual parity (comparing screenshots with a baseline) have proved
+	worthy to an extent.  Trying to simulate user actions thru code have been flaky, unstable and usually caused more test bugs than product bugs.
+	But this is my observation after having invested a few hours on the UI tests provided by XCode, I have written some comments in OutlookCalendarUITests.swift
+	on what failed in each of my attempts to test something.  Also, the best way to test UI is to USE the product !
+	
+5.  I need to integrate the weather service
